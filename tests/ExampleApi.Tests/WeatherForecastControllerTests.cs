66b470058ea992931cda7c0c12b7806f0f299228
Henk Sandoval <henkalexander.sandoval@gmail.com>
2024-01-12 18:56:04 +0100

    ✅ feat(WeatherForecastController): Implement logic that asserts that the returned data is valid.

--- tests/ExampleApi.Tests/WeatherForecastControllerTests.cs
+++ tests/ExampleApi.Tests/WeatherForecastControllerTests.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using System.Net.Http.Json;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
 
@@ -14,5 +15,21 @@ public class WeatherForecastControllerTests
 		var response = await httpClient.GetAsync("WeatherForecast");
 
 		response.StatusCode.Should().Be(HttpStatusCode.OK);
+	}
+
+	[Fact]
+	public async Task ShouldReturnValidForecastsOnGetRequest()
+	{
+		var httpClient = new WebApplicationFactory<Program>().CreateClient();
+
+		var response = await httpClient.GetFromJsonAsync<IEnumerable<WeatherForecast>>("WeatherForecast");
+
+		var expectedDate = DateOnly.FromDateTime(DateTime.Now);
+		response.Should().AllSatisfy(forecast =>
+		{
+			forecast.Date.Should().BeOnOrAfter(expectedDate);
+			forecast.TemperatureC.Should().BeInRange(-20, 55);
+			forecast.Summary.Should().NotBeNullOrWhiteSpace();
+		});
 	}
 }
